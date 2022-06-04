f1ea3cd79a1f54b9fe360c100fd46b34b03da466
猫老大 <176839299@qq.com>
2022-06-05 01:13:57 +0800

    add en page

---
+++ assets/js/lang.js
@@ -0,0 +1,17 @@
+
+  $(document).ready(function() {
+    if(navigator.language){
+        var lang = navigator.language;
+    }
+    else{
+        var lang = navigator.browserLanguage;
+    }
+    if(lang.indexOf('zh') > -1){
+        document.location.href = 'index.html';
+    }
+    else{
+        document.location.href = 'indexen.html';
+    }
+  });
+  
+  
